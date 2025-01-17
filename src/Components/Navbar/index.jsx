e672c2526eb3cbac6b1d15a7b26addb10cf78c66
Kevin Andres <kevinandres@ieee.org>
2025-01-16 22:04:43 -0500

    Comtador de productos en el carrito

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,6 +1,9 @@
+import { useContext } from "react";
 import { NavLink } from "react-router-dom";
+import {ShoppingCardContext} from '../../Context'
 
 const Navbar = () =>{
+    const context = useContext(ShoppingCardContext)
     const activeStyle = 'underline underline-offset-4'
     return (
         <nav className='flex justify-between items-center fixed z-10 top-0 w-full py-5 px-8 text-sm font-light'>
@@ -82,12 +85,7 @@ const Navbar = () =>{
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink to='/Blog' 
-                    className={({ isActive }) =>
-                        isActive ? activeStyle : undefined
-                      }>
-                    Carrito
-                    </NavLink>
+                    C {context.count}        
                 </li>
             </ul>
         </nav>
